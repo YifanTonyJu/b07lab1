3068e7f0c52cefb568f4824f8c2ba6d9e7101d7f
鞠逸凡 <juyifan@juyifandeMacBook-Pro.local>
2024-09-27 19:16:29 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,33 @@
+import java.io.File;
+
 public class Driver {
     public static void main(String [] args) {
             Polynomial p = new Polynomial();
             System.out.println(p.evaluate(3));
-            double [] c1 = {6,0,0,5};
-            Polynomial p1 = new Polynomial(c1);
-            double [] c2 = {0,-2,0,0,-9};
-            Polynomial p2 = new Polynomial(c2);
+            double [] c1 = {6,5};
+            int [] c1_f = {0,3};
+            Polynomial p1 = new Polynomial(c1, c1_f);
+            double [] c2 = {-2,-9};
+            int [] c2_f = {1, 4};
+            Polynomial p2 = new Polynomial(c2, c2_f);
             Polynomial s = p1.add(p2);
             System.out.println("s(0.1) = " + s.evaluate(0.1));
             if(s.hasRoot(1))
                     System.out.println("1 is a root of s");
             else
                     System.out.println("1 is not a root of s");
+            Polynomial t = p1.multiply(p2);
+            System.out.println("t(1) = " + t.evaluate(1));
+            System.out.println("t(2) = " + t.evaluate(2));
+            System.out.println("t(0) = " + t.evaluate(0));
+            p1.saveToFile("jyfnb");
+            File jyf = new File("jyfnb");
+            Polynomial j = new Polynomial(jyf);
+            Polynomial s2 = j.add(p2);
+            System.out.println("s2(0.1) = " + s2.evaluate(0.1));
+            if(s2.hasRoot(1))
+                    System.out.println("1 is a root of s2");
+            else
+                    System.out.println("1 is not a root of s2");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,73 +1,198 @@
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 public class Polynomial {
     double[] co;
+    int[] co_i;
 
     public Polynomial() {
         this.co = new double[1];
+        this.co_i = new int[1];
         return;
     }
 
-    public Polynomial(double[] coe) {
+    public Polynomial(double[] coe, int[] coe_i) {
         this.co = coe;
+        this.co_i = coe_i;
+        return;
+    }
+
+    public Polynomial(File f){
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String pol = scanner.nextLine();
+        String modPol = pol.replaceAll("-", "\\+-");
+        String[] terms = modPol.split("\\+");
+        int termNum = terms.length;
+        double[] coe = new double[termNum];
+        int[] fr = new int[termNum];
+        for (int i = 0; i < termNum; ++i) {
+            String ter = terms[i];
+            if(ter.contains("x")){
+                String[] cf = ter.split("x");
+                if (cf.length == 2){
+                    coe[i] = Double.parseDouble(cf[0]);
+                    fr[i] = Integer.parseInt(cf[1]);
+                }
+                else{
+                    coe[i] = Double.parseDouble(cf[0]);
+                    fr[i] = 1;
+                }
+            }
+            else{
+                coe[i] = Double.parseDouble(ter);
+                fr[i] = 0;
+            }
+        }
+        this.co = coe;
+        this.co_i = fr;
+        return;
+    }
+
+    public void saveToFile(String fn){
+        File file = new File(fn);
+        try {
+            if (file.createNewFile()){
+                String pol ="";
+                for(int i = 0; i < this.co.length - 1; ++i){
+                    if(co_i[i] == 0){
+                        pol = pol + co[i] + "+";
+                    }
+                    else if (co_i[i] == 1){
+                        pol = pol + co[i] + "x" + "+";
+                    }
+                    else{
+                        pol = pol + co[i] + "x" + co_i[i] + "+";
+                    }
+                }
+                pol = pol + co[this.co.length - 1] + "x" + co_i[this.co_i.length - 1];
+                pol = pol.replaceAll("\\+-", "-");
+                FileWriter fwr = new FileWriter(file);
+                BufferedWriter bwr = new BufferedWriter(fwr);
+                bwr.write(pol);
+                bwr.close();
+                return;
+            }
+            else{
+                String pol ="";
+                for(int i = 0; i < this.co.length - 1; ++i){
+                    if(co_i[i] == 0){
+                        pol = pol + co[i] + "+";
+                    }
+                    else if (co_i[i] == 1){
+                        pol = pol + co[i] + "x" + "+";
+                    }
+                    else{
+                        pol = pol + co[i] + "x" + co_i[i] + "+";
+                    }
+                }
+                pol = pol + co[this.co.length - 1] + "x" + co_i[this.co_i.length - 1];
+                pol = pol.replaceAll("\\+-", "-");
+                FileWriter fwr = new FileWriter(file);
+                BufferedWriter bwr = new BufferedWriter(fwr);
+                bwr.write(pol);
+                bwr.close();
+                return;
+            }
+        } catch(IOException e){
+            System.out.println(" Something Wrong Happend");
+            e.printStackTrace();
+        }
         return;
     }
 
     public Polynomial add(Polynomial a) {
-        int thisTermNum = this.co.length;
-        int aTermNum = a.co.length;
+        int thisLen = this.co_i[this.co_i.length - 1];
+        int aLen = a.co_i[a.co_i.length - 1];
+        int newLen = Math.max(thisLen, aLen) + 1;
 
-        if (thisTermNum < aTermNum) {
-            double[] nco = new double[aTermNum];
+        double[] add_co = new double[newLen];
 
-            for (int i = 0; i < thisTermNum; i++) {
-                nco[i] = a.co[i] + this.co[i];
+        for (int i = 0; i < this.co_i.length; ++i){
+            add_co[this.co_i[i]] = this.co[i];
+        }
+
+        for (int j = 0; j < a.co_i.length; ++j){
+            add_co[a.co_i[j]] = add_co[a.co_i[j]] + a.co[j];
+        }
+
+        int no_zero = 0;
+        for (int p = 0; p < newLen; ++p){
+            if (add_co[p] != 0){
+                no_zero += 1;
             }
+        }
 
-            for (int j = thisTermNum; j < aTermNum; j++) {
-                nco[j] = a.co[j];
+        double[] true_add_co = new double[no_zero];
+        int[] true_add_co_i = new int[no_zero];
+        int t = 0;
+        for(int k = 0; k < newLen; ++k){
+            if(add_co[k] != 0){
+                true_add_co[t] = add_co[k];
+                true_add_co_i[t] = k;
+                t += 1;
             }
-
-            Polynomial np = new Polynomial(nco);
-            return np;
         }
 
-        if (thisTermNum == aTermNum) {
-            double[] nco = new double[thisTermNum];
-
-            for (int i = 0; i < thisTermNum; i++) {
-                nco[i] = a.co[i] + this.co[i];
-            }
-
-            Polynomial np = new Polynomial(nco);
-            return np;
-        }
-
-        double[] nco = new double[aTermNum];
-
-        for (int i = 0; i < aTermNum; i++) {
-            nco[i] = a.co[i] + this.co[i];
-        }
-
-        for (int j = aTermNum; j < thisTermNum; j++) {
-            nco[j] = this.co[j];
-        }
-
-        Polynomial np = new Polynomial(nco);
-        return np;
+        Polynomial newp = new Polynomial(true_add_co, true_add_co_i);
+        return newp;
     }
 
     public double evaluate(double x) {
-        int thisTermNum = this.co.length;
-        double result = 0.0d;
+       int len = this.co.length;
+       double result = 0.0d;
 
-        for (int i = 0; i < thisTermNum; i++) {
-            result = result + this.co[i] * (Math.pow(x, i));
-        }
+       for (int i = 0; i < len; ++i){
+        result = result + this.co[i] * (Math.pow(x, this.co_i[i]));
+       }
 
-        return result;
+       return result;
     }
 
     public boolean hasRoot(double x) {
         double e = evaluate(x);
         return e == 0.0d;
+    }
+
+    public Polynomial multiply(Polynomial jyf) {
+        int thisLen = this.co_i[this.co_i.length - 1];
+        int jLen = jyf.co_i[jyf.co_i.length - 1];
+        int newLen = thisLen + jLen + 1;
+        double[] new_mul_co = new double[newLen];
+
+        for (int i = 0; i < this.co.length; ++i){
+            double ic = this.co[i];
+            for (int j = 0; j < jyf.co.length; ++j) {
+                new_mul_co[this.co_i[i] + jyf.co_i[j]] = new_mul_co[this.co_i[i] + jyf.co_i[j]] + ic * jyf.co[j];
+            }
+        }
+
+        int no_zero = 0;
+        for (int p = 0; p < newLen; ++p){
+            if (new_mul_co[p] != 0){
+                no_zero += 1;
+            }
+        }
+
+        double[] true_mul_co = new double[no_zero];
+        int[] true_mul_co_i = new int[no_zero];
+        int t = 0;
+        for(int k = 0; k < newLen; ++k){
+            if(new_mul_co[k] != 0){
+                true_mul_co[t] = new_mul_co[k];
+                true_mul_co_i[t] = k;
+                t += 1;
+            }
+        }
+        Polynomial nmp = new Polynomial(true_mul_co, true_mul_co_i);
+        return nmp;
     }
 }
